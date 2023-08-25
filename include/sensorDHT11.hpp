f08db7bb6e731d851364e7f424c9ed39a75d8862
Minerva <minervabotscontato@gmail.com>
2023-08-25 17:36:13 -0300

    refatorando o código e adicionando sensor de UV

--- include/sensorDHT11.hpp
+++ include/sensorDHT11.hpp
@@ -5,3 +5,23 @@ const int pinoDHT11 = A2;
 
 // Instanciando o objeto do tipo DHT
 dht DHT;
+
+
+void leituraSensorDHT11(){
+
+    // Lendo as informações diretamente do pino do sensor
+    DHT.read11(pinoDHT11); 
+
+    // Imprime o valor da Umidade
+    Serial.print("Umidade: ");
+    Serial.print(DHT.humidity); // Imprime o valor da umidade 
+    Serial.print("%"); // Adiciona o símbolo de porcentagem para representar a umidade
+
+    // Imprime o valor da Temperatura
+    Serial.print(" / Temperatura: ");
+    Serial.print(DHT.temperature, 0); // Imprime o valor da temperatura e retira a parte decimal
+    Serial.println("*C"); // Adiciona o símbolo de graus Celsius
+
+    // Adiciona um intervalo de 2 segundos - não diminuir esse valor
+    delay(2000);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "sensorDHT11.hpp"
+#include "sensorGuva-S12SD.hpp"
 
 void setup() {
 
@@ -8,23 +9,18 @@ void setup() {
 
   // Determina um intervalo de 2 segundos antes de iniciar o código
   delay(2000);
+
 }
 
 void loop() {
 
-  // Lendo as informações diretamente do pino do sensor
-  DHT.read11(pinoDHT11); 
+  // Chama a função de leitura do sensor de Umidade e Temperatura
+  leituraSensorDHT11();
 
-  // Imprime o valor da Umidade
-  Serial.print("Umidade: ");
-  Serial.print(DHT.humidity); // Imprime o valor da umidade 
-  Serial.print("%"); // Adiciona o símbolo de porcentagem para representar a umidade
-  
-  // Imprime o valor da Temperatura
-  Serial.print(" / Temperatura: ");
-  Serial.print(DHT.temperature, 0); // Imprime o valor da temperatura e retira a parte decimal
-  Serial.println("*C"); // Adiciona o símbolo de graus Celsius
-  
-  // Adiciona um intervalo de 2 segundos - não diminuir esse valor
-  delay(2000);
+  // Chama a função de leitura do sensor de UV
+  leituraSensorGuva();
+
 }
+
+
+ 
