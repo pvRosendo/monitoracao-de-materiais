0d7645e2d5b91d9e83286847c091a831ed5b8bca
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2023-08-29 02:40:21 -0300

    adicionando comentários e adaptando o código para 3 sensores DHT11

--- include/sensorDHT11.hpp
+++ include/sensorDHT11.hpp
@@ -1,17 +1,39 @@
+/*
+    Arquivo responsável por contar as funções de leitura dos sensores DHT11,
+    que são utilizados para medir a Umidade e a Temperatura do ambiente.
+
+*/
+
+// Importando a biblioteca do sensor
 #include <dht.h>
 
+
 // Definição do pino analógico utilizado pelo sensor DHT11
-const int pinoDHT11 = A2;
+const int pino_do_DHT11_de_fora = A2;
+const int pino_do_DHT11_de_dentro1 = A3;
+const int pino_do_DHT11_de_dentro2 = A4;
 
 // Instanciando o objeto do tipo DHT
 dht DHT;
 
 
-void leituraSensorDHT11(){
+/**
+ * @brief Função que realiza a leitura do sensor de Umidade e Temperatura
+ * @param pino_sensor_de_umidade_e_temperatura Pino analógico utilizado pelo sensor
+ * @param local_sensor Local onde o sensor está instalado fisicamente
+*/
+void leitura_sensores_DHT11(
+    const int pino_sensor_de_umidade_e_temperatura, 
+    String local_sensor
+){
 
     // Lendo as informações diretamente do pino do sensor
-    DHT.read11(pinoDHT11); 
+    DHT.read11(pino_sensor_de_umidade_e_temperatura); 
 
+    // Imprime qual o sensor que está sendo lido
+    Serial.print(local_sensor);
+    Serial.print(" - ");
+    
     // Imprime o valor da Umidade
     Serial.print("Umidade: ");
     Serial.print(DHT.humidity); // Imprime o valor da umidade 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+/*
+  Arquivo: main.cpp
+
+  Responsável por conter a base do projeto, ou seja, o código principal.
+
+*/
+
+
+// Importando os arquivos necessários
 #include <Arduino.h>
 #include "sensorDHT11.hpp"
 #include "sensorGuva-S12SD.hpp"
 
+
+/**
+ * @brief Função básica de configuração do framework Arduino. Responsável por iniciar o código
+ * 
+*/
 void setup() {
 
   // Inicializa o monitor serial (necessário para podermos usar "Serial.print")
@@ -12,10 +26,15 @@ void setup() {
 
 }
 
+/**
+ * @brief Função básica de loop do framework Arduino. Responsável por executar o código em loop
+*/
 void loop() {
 
-  // Chama a função de leitura do sensor de Umidade e Temperatura
-  leituraSensorDHT11();
+  // Chama as funções de leitura dos sensores de Umidade e Temperatura
+  leitura_sensores_DHT11(pino_do_DHT11_de_fora, "Sensor de Fora");
+  leitura_sensores_DHT11(pino_do_DHT11_de_dentro1, "Sensor de Dentro 1");
+  leitura_sensores_DHT11(pino_do_DHT11_de_dentro2, "Sensor de Dentro 2");
 
   // Chama a função de leitura do sensor de UV
   leituraSensorGuva();
