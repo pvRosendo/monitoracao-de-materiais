ffef6f9d4e2ffc4fab59a6d7673c037d21458c86
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2023-09-08 15:07:36 -0300

    validando  sensor UV

---
+++ include/sensor_UV.hpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+
+// Pino do sensor de UV
+int pino_sensor_UV = A3;
+
+// Variável que armazena o nível ultravioleta de acordo com a tabela
+String nivel_uv = "00";
+
+
+/**
+ * @brief Função responsável por iniciar o sensor ultravioleta
+ * 
+*/
+void iniciar_sensor_UV() {
+
+  // Seta o pino do sensor como entrada
+  pinMode(pino_sensor_UV, INPUT);
+
+}
+
+
+/**
+ * @brief Função responsável por ler o nível ultravioleta
+ * 
+*/
+void ler_sensor_UV(){
+  
+  // Guarda na variável a leitura do sensor
+  float valor_sensor_UV = analogRead(pino_sensor_UV);
+  Serial.println(valor_sensor_UV);
+
+  //Calcula tensao em milivolts para seguir a tabela
+  float tensao_convertida = (valor_sensor_UV*(3.3/1023.0))*1000; 
+  Serial.println(tensao_convertida);
+  Serial.println("************************************");
+  
+  //Compara a tensão calculada com valores tabela do nível ultravioleta
+  if(tensao_convertida <= 227) nivel_uv = "00";
+  else if (tensao_convertida > 227 && tensao_convertida <= 318)   nivel_uv = "01";
+  else if (tensao_convertida > 318 && tensao_convertida <= 408)   nivel_uv = "02";
+  else if (tensao_convertida > 408 && tensao_convertida <= 503)   nivel_uv = "03";
+  else if (tensao_convertida > 503 && tensao_convertida <= 606)   nivel_uv = "04";
+  else if (tensao_convertida > 606 && tensao_convertida <= 696)   nivel_uv = "05";
+  else if (tensao_convertida > 696 && tensao_convertida <= 795)   nivel_uv = "06";
+  else if (tensao_convertida > 795 && tensao_convertida <= 881)   nivel_uv = "07";
+  else if (tensao_convertida > 881 && tensao_convertida <= 976)   nivel_uv = "08";
+  else if (tensao_convertida > 976 && tensao_convertida <= 1079)  nivel_uv = "09";
+  else if (tensao_convertida > 1079 && tensao_convertida <= 1170) nivel_uv = "10";
+  else nivel_uv = "11";
+
+  
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,18 @@ void setup() {
   // Inicializa o monitor serial (necessário para podermos usar "Serial.print")
   Serial.begin(9600);
 
+  // Inicializa o sensor de UV
+  iniciar_sensor_UV();
+
   // Inicializa os sensores DHT
-  dht1.begin();
-  dht2.begin();
-  dht3.begin();
+  // dht1.begin();
+  // dht2.begin();
+  // dht3.begin();
 
   // Iniciando os sensores de Umidade e Temperatura
-  iniciar_sensores_DHT(sensor_DHT_de_fora,    "Sensor de Fora",     1);
-  iniciar_sensores_DHT(sensor_DHT_de_dentro1, "Sensor de Dentro 1", 2);
-  iniciar_sensores_DHT(sensor_DHT_de_dentro2, "Sensor de Dentro 2", 3);
+  // iniciar_sensores_DHT(sensor_DHT_de_fora,    "Sensor de Fora",     1);
+  // iniciar_sensores_DHT(sensor_DHT_de_dentro1, "Sensor de Dentro 1", 2);
+  // iniciar_sensores_DHT(sensor_DHT_de_dentro2, "Sensor de Dentro 2", 3);
 
   // Determina um intervalo de 2 segundos antes de iniciar o código
   delay(2000);
@@ -42,13 +45,14 @@ void setup() {
 void loop() {
 
   // Chama as funções de leitura dos sensores de Umidade e Temperatura
-  leitura_dos_sensores_DHT(evento_DHT_de_fora,    "Sensor de Fora",     1);
-  leitura_dos_sensores_DHT(evento_DHT_de_dentro1, "Sensor de Dentro 1", 2);
-  leitura_dos_sensores_DHT(evento_DHT_de_dentro2, "Sensor de Dentro 2", 3);
+  // leitura_dos_sensores_DHT(evento_DHT_de_fora,    "Sensor de Fora",     1);
+  // leitura_dos_sensores_DHT(evento_DHT_de_dentro1, "Sensor de Dentro 1", 2);
+  // leitura_dos_sensores_DHT(evento_DHT_de_dentro2, "Sensor de Dentro 2", 3);
 
   // Chama a função de leitura do sensor de UV
-  leituraSensorGuva();
+  ler_sensor_UV();
 
+  Serial.println(nivel_uv);
 }
 
 
